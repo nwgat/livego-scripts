962c6b2c6ab77f84a2e3ee799aa39c001ffa6f98
nwgat <github@nwgat.net>
2021-04-06 13:03:30 +0200

    Update live.sh

--- live.sh
+++ live.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-ffmpeg -loglevel error -re -f video4linux2 -input_format h264 -video_size 1280x720 -framerate 30 -i /dev/video0 -vcodec copy -an -strict -2 -f flv rtmp://192.168.1.2:1935/live/keyfromlivego
+
+# rtmp key
+key="key"
+# rtmp ip
+ip="livego"
+# resolution
+res="1280x720"
+# framerate
+fps="30"
+# birrate
+bt="1000000"
+
+v4l2-ctl --set-ctrl video_bitrate=$bt
+ffmpeg -loglevel error -re -f video4linux2 -input_format h264 -video_size $res -framerate $fps -i /dev/video0 -vcodec copy -an -strict -2 -f flv rtmp://$ip:1935/live/$key
